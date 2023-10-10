72e7e2aabed7655e4e8293d2162730227ffab10f
Lucas Danburg <lidanburg@icloud.com>
2023-10-10 11:54:11 -0400

    ok im gonna try to do the trimming and cropping and shit all manally on a canvas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getFrames } from "./static/lib/data.js";
+import { trim } from "./static/lib/data.js";
 
 const squat = document.getElementById('squat');
 const bench = document.getElementById('bench');
@@ -31,31 +31,37 @@ const video = document.getElementById('playback');
 const uploadbtn = document.getElementById('uploadbtn')
 
 input.addEventListener('change', handleFileUpload);
-
-function handleFileUpload(event) {
+async function handleFileUpload(event) {
     const file = event.target.files[0];
+    console.log('uploaded')
     if (file) {
-        const objectURL = URL.createObjectURL(file);
+        var newfile = await trim(file);
+        console.log('trimmed')
+        // first place the video in the video element
+        // i think this is needed to read it into something
+        const objectURL = URL.createObjectURL(newfile);
         video.src = objectURL;
-
         video.style.display = "block";
         uploadbtn.style.display = "none";
         input.style.display = "none";
     }
+    return null;
 }
 
-async function run() {
-    console.log('started load')
-    const model = await tf.loadLayersModel('static/models/squat/model.json');
+const subbtn = document.getElementById('subbtn');
+subbtn.addEventListener('click', handleSubmit);
+async function handleSubmit(event) {
+    console.log('submitted')
+    // our two parameters here are *video* - the HTML <video> element containing the processed submition
+    // and the type of video (s/b/d), determined by the currently selected button
+    var model = 0;
+    if (squat.checked) { model = await tf.loadLayersModel('static/models/squat/model.json'); }
+    else if (bench.checked) { model = await tf.loadLayersModel('static/models/bench/model.json'); }
+    else if (deadlift.checked) { model = await tf.loadLayersModel('static/models/deadlift/model.json'); }
 
     const circ1 = document.getElementById('light1')
     const circ2 = document.getElementById('light2')
     const circ3 = document.getElementById('light3')
 
-    circ1.style.backgroundColor = '#000000';
-    console.log('finished load')
-    console.log('predicting')
-    const frames = getFrames('squat-vid1-4_3.mp4')
+    // SPECIFICALLY DISCARD THE MODEL FROM MEMORY AT THE END OF THIS FUNCTION
 }
-
-run();
--- src/static/lib/data.js
+++ src/static/lib/data.js
@@ -1,63 +1,9 @@
-import { SECS, FPS } from './CONSTANTS.js'
+import { SECS, FPS, SIZE } from './CONSTANTS.js';
 
-async function getFrames(path, secs = SECS, fps = FPS) {
-    // Create a VideoCapture object
-    const video = new cv.VideoCapture();
-    await video.openVideo(path);
-  
-    // Calculate the total number of frames to read
-    const totalFrames = secs * fps;
-  
-    // Initialize an array to store frames
-    const frames = new Array(totalFrames);
-  
-    // Initialize variables for frame and video indices
-    let framesIndex = 0;
-    let videoIndex = 0;
-  
-    // Calculate the frame stepping based on video's original FPS and target FPS
-    const stepper = Math.floor(video.get(cv.CAP_PROP_FPS) / fps);
-  
-    // Loop to read frames
-    while (framesIndex < totalFrames) {
-        video.set(cv.CAP_PROP_POS_FRAMES, videoIndex);
-        const frame = new cv.Mat();
-        const success = video.read(frame);
-
-        if (!success) {
-            break;
-        }
-
-        cv.cvtColor(frame, frame, cv.COLOR_BGR2GRAY);
-        frames[framesIndex] = frame.clone();
-        frame.delete();
-
-        videoIndex += stepper;
-        framesIndex += 1;
-    }
-  
-    video.delete();
+async function trim(file) {
     
-    // Convert frames to a single 4D tensor
-    const frameShape = new cv.Size(frames[0].cols, frames[0].rows);
-    const frameCount = frames.length;
-    const channels = 1; // Grayscale image
-    const depth = cv.CV_8U;
-    const frameData = new Uint8Array(frameCount * frameShape.height * frameShape.width * channels);
-  
-    for (let i = 0; i < frameCount; i++) {
-        const frame = frames[i];
-        const view = new Uint8Array(frame.data);
-        frameData.set(view, i * frameShape.height * frameShape.width * channels);
-        frame.delete();
-    }
-  
-    const framesMat = new cv.Mat(frameCount, frameShape.height * frameShape.width * channels, depth);
-    framesMat.data.set(frameData);
-  
-    return framesMat;
 }
 
 export {
-    getFrames
+    trim
 }
